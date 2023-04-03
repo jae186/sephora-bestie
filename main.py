e76a4a1a5709a2055f2f398b8933052aacd0751c
Jayati <jae186@gmail.com>
2023-04-02 20:18:24 -0400

    script for sephora bestie done and encrypted :)

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import asyncio
-import requests
+import os
 import telegram
 from bs4 import BeautifulSoup
+import aiohttp
 
+telegram_bot_token = os.environ['TELEGRAM_BOT_TOKEN']
+chat_id = os.environ['CHAT_ID']
 
-bot = telegram.Bot(token='6238012639:AAGrE_gmnMr1DoygZ2iD-Zw-OUvNUvJeX7Q')
-chat_id = '6023747101'
+bot = telegram.Bot(token=telegram_bot_token)
 
 # URL of the website to scrape
 url = 'https://www.sephora-events.com/'
 
 async def my_function():
-    # Perform the web scraping and link checking
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            content = await response.text()
+
+    soup = BeautifulSoup(content, 'html.parser')
     zoom_links = []
     for link in soup.find_all('a'):
         href = link.get('href')
@@ -22,12 +26,9 @@ async def my_function():
 
     # Send a notification if new links are available
     zoom_links = set(zoom_links)
-    if len(zoom_links) > 1:
+    if len(zoom_links) > 2:
         message = "New links available: " + str(zoom_links)
         await bot.send_message(chat_id=chat_id, text=message)
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(my_function())
-
-    
-
